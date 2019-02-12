6083656512e3733f3023efad58aee173cb9b049b
Alexis Daniel Zazueta Ortega <alexisdaniel.zazueta.next@bbva.com>
2019-02-12 17:17:00 -0600

    Added the RDS configuration

--- variables.tf
+++ variables.tf
@@ -1,9 +1,13 @@
-variable "aws_region" {}
 variable "aws_profile" {}
-data "aws_availability_zones" "availables" {}
+variable "aws_region" {}
 variable "vpc_cidr" {}
 variable "localip" {}
+variable "domain_name" {}
+variable "db_instance_class" {}
+variable "dbname" {}
+variable "dbuser" {}
+variable "dbpassword" {}
+data "aws_availability_zones" "availables" {}
 variable "cidrs" {
   type = "map"
 }
-variable "domain_name" {}
