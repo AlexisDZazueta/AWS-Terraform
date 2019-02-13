7244d0b80125e763a5c76083f28463052feb3011
Alexis Daniel Zazueta Ortega <alexisdaniel.zazueta.next@bbva.com>
2019-02-13 10:50:15 -0600

    Added Dev Server

--- main.tf
+++ main.tf
@@ -351,7 +351,24 @@ resource "aws_instance" "wp_dev" {
   tags {
     Name = "wp_dev"
   }
-  key_name = 
+  key_name = "${aws_key_pair.wp_auth.id}"
+  vpc_security_group_ids = ["${aws_security_group.wp_dev_sg.id}"]
+  iam_instance_profile = "${aws_iam_instance_profile.s3_access_profile.id}"
+  subnet_id = "${aws_subnet.wp_public1_subnet.id}"
+  provisioner "local-exec" {
+    command = <<EOD
+      cat <<EOF > aws_hosts
+        [dev]
+        ${aws_instance.wp_dev.public_ip}
+        [dev:vars]
+        s3code=${aws_s3_bucket.code.bucket}
+        domain=${var.domain_name}
+      EOF
+    EOD
+  }
+  provisioner "local-exec" {
+    command = "aws ec2 wait instance-status-ok --instance-ids ${aws_instance.wp_dev.id} --profile ${var.aws_profile} && ansible-playbook -i aws_hosts wordpress.yml"
+  } 
 }
 
 
