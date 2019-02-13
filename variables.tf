95f831c1cc34a39314d2e1603805ff861ed98f46
Alexis Daniel Zazueta Ortega <alexisdaniel.zazueta.next@bbva.com>
2019-02-13 09:41:58 -0600

    Added ELB

--- variables.tf
+++ variables.tf
@@ -7,6 +7,14 @@ variable "db_instance_class" {}
 variable "dbname" {}
 variable "dbuser" {}
 variable "dbpassword" {}
+variable "dev_instance_type" {}
+variable "dev_ami" {}
+variable "public_key_path" {}
+variable "key_name" {}
+variable "elb_healthy_threashold" {}
+variable "elb_unhealthy_threashold" {}
+variable "elb_timeout" {}
+variable "elb_interval" {}
 data "aws_availability_zones" "availables" {}
 variable "cidrs" {
   type = "map"
